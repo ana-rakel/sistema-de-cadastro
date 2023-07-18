045cb45c24701af7849e150f5d08bccd123c963d
Ana Rakel Cardoso <arakelcardoso@uol.com.br>
2023-07-18 17:55:02 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log{"Rodando o sistema de Gerenciamento de Jogos"}
 console.log{"Aplicação está se conectando ao banco de dados"}
+console,log{"Fazendo teste de commit para mais de um arequivo"}
